0aba5a5fca68d1ddda49ef4815d5e9914ce08746
Acrylic125 <acrylic125email@gmail.com>
2023-02-23 22:07:37 +0800

    done setup

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,13 +7,13 @@ export const authOptions: NextAuthOptions = {
   providers: [],
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    // async jwt({ token, account }) {
-    //   // Persist the OAuth access_token to the token right after signin
-    //   if (account) {
-    //     token.accessToken = account.access_token;
-    //   }
-    //   return token;
-    // },
+    async jwt({ token, account, isNewUser, user, profile }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
     // async session({ session, token, user }) {
     //   // Send properties to the client, like an access_token from a provider.
     //   session.accessToken = token.accessToken;
