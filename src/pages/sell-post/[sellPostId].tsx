19bd5aa807b3153d37f694cd5b6d51a66c61a52a
Acrylic125 <acrylic125email@gmail.com>
2023-02-26 14:48:41 +0800

    wip

---
+++ src/pages/sell-post/[sellPostId].tsx
@@ -0,0 +1,12 @@
+import MainLayout from "@/components/MainLayout";
+import { useRouter } from "next/router";
+
+const SellPost = () => {
+  const router = useRouter();
+
+  return <MainLayout>
+    
+  </MainLayout>;
+};
+
+export default SellPost;
