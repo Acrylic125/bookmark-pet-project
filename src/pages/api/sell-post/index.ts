46a027b013a6b94921eb847201565059319effee
Acrylic125 <acrylic125email@gmail.com>
2023-02-26 14:48:56 +0800

    feat: api routes to create and get sell posts

---
+++ src/pages/api/sell-post/index.ts
@@ -0,0 +1,54 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import client from "@/utils/prisma";
+import { SellPost } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+const SellPostGetBody = z.object({});
+const CreateSellPostBody = z.object({
+  name: z.string().min(8).max(128),
+  description: z.string().min(0).max(1024),
+  status: z.enum(["active", "inactive"]),
+});
+
+async function POST(req: NextApiRequest, res: NextApiResponse<SellPost>) {
+  const body = CreateSellPostBody.parse(req.body);
+
+  console.log(body);
+  try {
+    const created = await client.sellPost.create({
+      data: {
+        name: body.name,
+        description: body.description,
+        status: body.status,
+      },
+    });
+    res.status(200).json(created);
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
+}
+
+async function GET(req: NextApiRequest, res: NextApiResponse<SellPost[]>) {
+  const body = SellPostGetBody.parse(req.body);
+
+  try {
+    const sellPosts = await client.sellPost.findMany();
+    res.status(200).json(sellPosts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case "POST":
+      return await POST(req, res);
+    case "GET":
+      return await GET(req, res);
+    default:
+      res.status(405).end();
+  }
+}
