cb28dd74d62f41107ba4feb130f46e8bcb20fa7b
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2023-02-26 21:18:46 +0800

    sign in fixes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/utils/prisma";
+import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -12,7 +12,7 @@ export const authOptions: NextAuthOptions = {
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
+      async authorize(credentials) {
         if (!credentials) {
           return null;
         }
@@ -24,9 +24,15 @@ export const authOptions: NextAuthOptions = {
           },
         });
 
-        if (user) {
+        if (user && user.password) {
+          // Check  if the password is correct
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
           // Any object returned will be saved in the `user` property of the JWT
-          return user;
+          return isPasswordCorrect ? user : null;
         } else {
           // If you return null, an error will be displayed advising the user to check their details.
           return null;
