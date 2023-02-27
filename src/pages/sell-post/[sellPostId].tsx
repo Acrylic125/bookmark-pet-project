e0a5b8fcaf7f50dd1b01338a9e88ba55f90ce4c2
Acrylic125 <acrylic125email@gmail.com>
2023-02-27 20:56:53 +0800

    feat: sell post page

--- src/pages/sell-post/[sellPostId].tsx
+++ src/pages/sell-post/[sellPostId].tsx
@@ -1,12 +1,100 @@
 import MainLayout from "@/components/MainLayout";
+import { Container, Divider, Paper, Stack, Typography } from "@mui/material";
+import { type SellPost as SellPostType } from "@prisma/client";
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+import { z } from "zod";
 
-const SellPost = () => {
-  const router = useRouter();
+const SellPostSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  status: z.enum(["active", "inactive"]),
+});
 
-  return <MainLayout>
-    
-  </MainLayout>;
+const SellPost = ({ sellPost }: { sellPost: z.infer<typeof SellPostSchema> }) => {
+  const {
+    query: { sellPostId },
+  } = useRouter();
+  // const { data: sellPost, isLoading: sellPostIsLoading } = useQuery(["sellPost", sellPostId], async () => {
+  //   const resp = await fetch(`/api/sell-post/${sellPostId}`, {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   if (!resp.ok) {
+  //     throw new Error("Failed to fetch sell post");
+  //   }
+  //   return resp.json();
+  // });
+
+  return (
+    <MainLayout>
+      <Container
+        sx={{
+          padding: ({ spacing }) => spacing(4),
+        }}
+        maxWidth="xl"
+      >
+        <Stack gap={1}>
+          <Typography
+            sx={{
+              fontWeight: "bold",
+            }}
+            component="h1"
+            variant="h4"
+          >
+            {sellPost.name}
+          </Typography>
+          <Typography component="p" variant="subtitle1">
+            {sellPost.description}
+          </Typography>
+
+          <Divider />
+
+          <Stack gap={1}>
+            <Typography
+              sx={{
+                fontWeight: "bold",
+              }}
+              component="h2"
+              variant="h6"
+            >
+              Seller Actions
+            </Typography>
+          </Stack>
+        </Stack>
+      </Container>
+    </MainLayout>
+  );
 };
 
 export default SellPost;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { sellPostId } = context.query;
+  const url =
+    process.env.NODE_ENV === "development"
+      ? `http://${context.req?.headers.host}/api/sell-post/${sellPostId}`
+      : `https://${context.req?.headers.host}/api/sell-post/${sellPostId}`;
+  const resp = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!resp.ok) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const sellPost = await resp.json();
+  return {
+    props: {
+      sellPost: SellPostSchema.parse(sellPost),
+    },
+  };
+};
