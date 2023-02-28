2bac0244b7d82f3a057c6b0d17d36407462f74f7
Acrylic125 <acrylic125email@gmail.com>
2023-02-28 16:05:10 +0800

    feat: update bookmark sell post status

--- src/pages/sell-post/[sellPostId].tsx
+++ src/pages/sell-post/[sellPostId].tsx
@@ -1,22 +1,63 @@
 import MainLayout from "@/components/MainLayout";
-import { Container, Divider, Paper, Stack, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Container,
+  Divider,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { type SellPost as SellPostType } from "@prisma/client";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
-import { useQuery } from "react-query";
+import { useState } from "react";
+import { useMutation, useQuery } from "react-query";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 import { z } from "zod";
 
 const SellPostSchema = z.object({
   id: z.string(),
   name: z.string(),
   description: z.string(),
-  status: z.enum(["active", "inactive"]),
+  status: z.enum(["available", "sold_out"]),
 });
 
-const SellPost = ({ sellPost }: { sellPost: z.infer<typeof SellPostSchema> }) => {
+const SellPost = ({ bookmarked: defaultBookmarked, sellPost }: { bookmarked: boolean; sellPost: z.infer<typeof SellPostSchema> }) => {
   const {
     query: { sellPostId },
   } = useRouter();
+  const [bookmarked, setBookmarked] = useState<boolean>(defaultBookmarked);
+  const [status, setStatus] = useState<SellPostType["status"]>(sellPost.status);
+
+  const {
+    isLoading: updateBookmarkIsLoading,
+    error: updateBookmarkError,
+    isSuccess: updateBookmarkIsSuccess,
+    mutate: updateBookmark,
+  } = useMutation(async (newStatus: SellPostType["status"]) => {
+    const resp = await fetch(`/api/sell-post/${sellPostId}/bookmark`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        status: newStatus,
+      }),
+    });
+    if (!resp.ok) {
+      throw new Error("Failed to update sell post");
+    }
+    return resp.json();
+  });
+
   // const { data: sellPost, isLoading: sellPostIsLoading } = useQuery(["sellPost", sellPostId], async () => {
   //   const resp = await fetch(`/api/sell-post/${sellPostId}`, {
   //     method: "GET",
@@ -39,32 +80,96 @@ const SellPost = ({ sellPost }: { sellPost: z.infer<typeof SellPostSchema> }) =>
         maxWidth="xl"
       >
         <Stack gap={1}>
-          <Typography
+          <Stack
             sx={{
-              fontWeight: "bold",
+              width: "100%",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
-            component="h1"
-            variant="h4"
+            direction="row"
           >
-            {sellPost.name}
-          </Typography>
-          <Typography component="p" variant="subtitle1">
-            {sellPost.description}
-          </Typography>
-
-          <Divider />
-
-          <Stack gap={1}>
             <Typography
               sx={{
                 fontWeight: "bold",
               }}
-              component="h2"
-              variant="h6"
+              component="h1"
+              variant="h4"
             >
-              Seller Actions
+              {sellPost.name}
             </Typography>
+            <IconButton>{bookmarked ? <BookmarkIcon color="primary" /> : <BookmarkBorderIcon color="inherit" />}</IconButton>
           </Stack>
+
+          <Typography component="p" variant="subtitle1">
+            {sellPost.description}
+          </Typography>
+
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              updateBookmark(status);
+            }}
+          >
+            <Stack gap={1}>
+              <Typography
+                sx={{
+                  fontWeight: "bold",
+                  color: "grey.700",
+                }}
+                component="h2"
+                variant="h5"
+              >
+                Listing Settings
+              </Typography>
+              <Divider />
+              <Stack
+                sx={{
+                  width: "100%",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  paddingY: ({ spacing }) => spacing(1),
+                }}
+                direction="row"
+                gap={1}
+              >
+                <Typography
+                  sx={{
+                    fontWeight: "bold",
+                    color: "grey.400",
+                  }}
+                  component="h2"
+                  variant="h6"
+                >
+                  Status
+                </Typography>
+                <FormControl>
+                  <InputLabel id="status">Status</InputLabel>
+                  <Select
+                    value={status}
+                    onChange={(e) => {
+                      setStatus(e.target.value as SellPostType["status"]);
+                    }}
+                    labelId="status"
+                    label="Status"
+                  >
+                    <MenuItem value="available">Available</MenuItem>
+                    <MenuItem value="sold_out">Sold Out</MenuItem>
+                  </Select>
+                </FormControl>
+              </Stack>
+              <>
+                {updateBookmarkError && (
+                  <Alert severity="error">{updateBookmarkError instanceof Error ? updateBookmarkError.message : "An unknown error occured."}</Alert>
+                )}
+                {updateBookmarkIsSuccess && <Alert severity="success">Updated Sell Post!</Alert>}
+              </>
+              <Box>
+                <Button type="submit" size="large" variant="contained" disabled={updateBookmarkIsLoading}>
+                  Update
+                </Button>
+              </Box>
+            </Stack>
+          </form>
         </Stack>
       </Container>
     </MainLayout>
@@ -75,23 +180,33 @@ export default SellPost;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { sellPostId } = context.query;
-  const url =
-    process.env.NODE_ENV === "development"
-      ? `http://${context.req?.headers.host}/api/sell-post/${sellPostId}`
-      : `https://${context.req?.headers.host}/api/sell-post/${sellPostId}`;
-  const resp = await fetch(url, {
+  const baseUrl = process.env.NODE_ENV === "development" ? `http://${context.req?.headers.host}` : `https://${context.req?.headers.host}`;
+  const sellPostResp = await fetch(`${baseUrl}/api/sell-post/${sellPostId}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
   });
-  if (!resp.ok) {
+  if (!sellPostResp.ok) {
     return {
       notFound: true,
     };
   }
+  const sellPost = await sellPostResp.json();
 
-  const sellPost = await resp.json();
+  // const userBookmark = await fetch(`${baseUrl}/api/sell-post/${sellPostId}`, {
+  //   method: "GET",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
+  // if (!sellPostResp.ok) {
+  //   return {
+  //     notFound: true,
+  //   };
+  // }
+  // const sellPost = await sellPostResp.json();
+
   return {
     props: {
       sellPost: SellPostSchema.parse(sellPost),
