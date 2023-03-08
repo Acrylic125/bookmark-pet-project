d6ef6cad01e4f3cae1a3cfd717315083efcc4702
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2023-03-08 08:23:30 +0800

    more sophisticated notification system

---
+++ src/pages/api/notification/bulk-notification-v2.ts
@@ -0,0 +1,173 @@
+import client from "@/utils/prisma";
+import { Response } from "@/types/Response";
+import {
+  SellPost,
+  SellPostUserBookmark,
+  User,
+  UserNotification,
+} from "@prisma/client";
+import sendNotificationEmail from "./bulkSendNotificationEmails";
+import { BulkEmailRequestBody } from "@/types/BulkEmailRequestBody";
+
+export default async function sendBulkNotifications(
+  data: UserNotification[]
+): Promise<Response> {
+  /* Adds all the notifications to the database.
+    If the status of the post changes back to the original, then remove the notification from the database.
+    If the status of the post changes to a different status, just update the notification in the database.
+    If notifications were queued 5 minutes or more ago, send emails with SIB and then remove the notifications from the database.
+    If notifications were queued less than 5 minutes ago and there have been no changes to the post, do nothing.
+    */
+
+  // Fetch all the notifications from the database
+  const existingNotifications = await client.userNotification.findMany({
+    include: {
+      User: true,
+      SellPost: true,
+    },
+  });
+
+  // Add all the notifications to the database, skipping duplicates
+  await client.userNotification.createMany({
+    data: data,
+    skipDuplicates: true, // Uniqueness is determined by the userId
+  });
+
+  // Find all the notifications that exist in both arrays
+  const notificationsToBeUpdated = existingNotifications.filter(
+    (existingNotification) => {
+      return data.some(
+        (notification) => notification.userId === existingNotification.userId
+      );
+    }
+  );
+
+  notificationsToBeUpdated.map(async (notification) => {
+    // Find the notification in the array of notifications to be added
+    const notificationToBeAdded = data.find(
+      (notification) => notification.userId === notification.userId
+    );
+
+    if (notificationToBeAdded === undefined) {
+      return;
+    }
+
+    /* For the purposes of this demo project, we assume there are only two statuses: available and sold_out.
+    If the status of the post changes at all, it logically means that the post is now back to the original status.
+    This means the user does not need to be notified of the change, so we remove the notification from the database.
+    
+    In an actual production application, there would likely be more statuses, and the user would need to be notified of the change.
+    In this case, we would require two db columns: originalPostStatus and currentPostStatus.
+    */
+    if (
+      notificationToBeAdded.originalPostStatus !==
+      notification.originalPostStatus
+    ) {
+      // Remove the notifications from the database
+      await client.userNotification.delete({
+        where: {
+          id: notification.id,
+        },
+      });
+    }
+
+    /*
+    // In a production app, we would update the currentPostStatus column with something like this:
+    await client.userNotification.update({
+      where: {
+        id: notification.id,
+      },
+      data: {
+        updatedAt: new Date(),
+        originalPostStatus: notification.originalPostStatus,
+      },
+    });
+    */
+  });
+
+  // Filter out all the notifications that were queued less than 5 minutes ago
+  const notificationsToSend = existingNotifications.filter((notification) => {
+    return notification.updatedAt === null
+      ? false
+      : notification.updatedAt < new Date(Date.now() - 5 * 60 * 1000);
+  });
+
+  // Create the BulkEmailRequestBody
+  const body: BulkEmailRequestBody = {
+    messageVersions: [], // To understand why this is an array, see the comment in /api/notification/bulkSendNotificationEmails.ts
+  };
+
+  // Separate the notifications by user, so that we can lump all the updates into a single email
+  const notificationsByUser: {
+    [key: string]: (UserNotification & {
+      User: User;
+      SellPost: SellPost;
+    })[];
+  } = {};
+
+  notificationsToSend.map((notification) => {
+    if (notificationsByUser[notification.userId] === undefined) {
+      notificationsByUser[notification.userId] = [];
+    }
+    notificationsByUser[notification.userId].push(notification);
+  });
+
+  // Create the message versions
+  const messageVersions = Object.keys(notificationsByUser).map(
+    async (userId) => {
+      const userNotifications = notificationsByUser[userId];
+
+      const message: string = userNotifications
+        .map((notification) => {
+          return `The sell post <h5>"${
+            notification.SellPost.name
+          }"</h5> is now "${
+            notification.SellPost.status === "available"
+              ? "Available"
+              : "Sold Out"
+          }". <a href="http://localhost:3000/sell-post/${
+            notification.SellPost.id
+          }">Click to check it out.</a>`;
+        })
+        .join("<br><br>");
+
+      const user = notificationsByUser[userId][0].User as User;
+
+      if (user === null || user.email === null || user.name === null) {
+        return;
+      }
+
+      // Create the message version
+      body.messageVersions.push({
+        to: [
+          {
+            email: user.email,
+            name: user.name,
+          },
+        ],
+        params: {
+          name: user.name,
+          message: message,
+        },
+      });
+    }
+  );
+
+  // Wait for all the message versions to be created
+  await Promise.all(messageVersions);
+
+  const emailResponse = await sendNotificationEmail(body);
+
+  if (emailResponse.success) {
+    // Remove the notifications from the database
+    await client.userNotification.deleteMany({
+      where: {
+        id: {
+          in: notificationsToSend.map((notification) => notification.id),
+        },
+      },
+    });
+  }
+
+  return emailResponse;
+}
--- src/pages/api/sell-post/[sellPostId]/bookmark/index.ts
+++ src/pages/api/sell-post/[sellPostId]/bookmark/index.ts
@@ -1,10 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import client from "@/utils/prisma";
-import { SellPost, SellPostUserBookmark, User } from "@prisma/client";
+import {
+  SellPost,
+  SellPostUserBookmark,
+  User,
+  UserNotification,
+} from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 import { BulkEmailRequestBody } from "@/types/BulkEmailRequestBody";
 import sendNotificationEmail from "@/pages/api/notification/bulkSendNotificationEmails";
+import sendBulkNotifications from "@/pages/api/notification/bulk-notification-v2";
 
 const UpdateSellPostBookmarkBody = z.object({
   status: z.enum(["available", "sold_out"]),
@@ -30,50 +36,66 @@ async function PATCH(
     });
 
     // Fetch all users from the User table who have bookmarked this sell post
-    const results: (SellPostUserBookmark & { user: User })[] =
-      await client.sellPostUserBookmark.findMany({
-        where: {
-          sellPostId: sellPostId,
-        },
-        include: {
-          user: true,
-        },
-      });
-
-    const emailRequestBody: BulkEmailRequestBody = {
-      messageVersions: [],
-    };
-
-    const emailRequests = results.map((result) => {
-      const user = result.user;
-      if (!user.email) {
-        console.log("User does not have an email:", user);
-        return;
-      }
-
-      console.log("Notifying user:", user.email);
-
-      emailRequestBody.messageVersions.push({
-        // To understand why this is an array, see the comment in /api/notification/bulkSendNotificationEmails.ts
-        to: [
-          {
-            email: user.email,
-            name: user.name ?? "SIWMA User",
-          },
-        ],
-        params: {
-          name: user.name ?? "SIWMA User",
-          message: `The sell post <h5>"${updated.name}"</h5> is now "${
-            updated.status === "available" ? "Available" : "Sold Out"
-          }".<br><br><a href="http://localhost:3000/sell-post/${
-            updated.id
-          }">Click to check it out.</a>`,
-        },
-      });
+    const results: (SellPostUserBookmark & { user: User } & {
+      sellPost: SellPost;
+    })[] = await client.sellPostUserBookmark.findMany({
+      where: {
+        sellPostId: sellPostId,
+      },
+      include: {
+        user: true,
+        sellPost: true,
+      },
     });
 
-    const emailResponse = await sendNotificationEmail(emailRequestBody);
+    const notificationRequests: UserNotification[] = [];
+    results.map((result) =>
+      notificationRequests.push({
+        id: "",
+        updatedAt: null,
+        userId: result.userId,
+        originalPostStatus: result.sellPost.status,
+        sellPostId: result.sellPostId,
+      })
+    );
+
+    const emailResponse = await sendBulkNotifications(notificationRequests);
     console.log("Email response:", emailResponse.success);
+
+    // const emailRequestBody: BulkEmailRequestBody = {
+    //   messageVersions: [],
+    // };
+
+    // const emailRequests = results.map((result) => {
+    //   const user = result.user;
+    //   if (!user.email) {
+    //     console.log("User does not have an email:", user);
+    //     return;
+    //   }
+
+    //   console.log("Notifying user:", user.email);
+
+    //   emailRequestBody.messageVersions.push({
+    //     // To understand why this is an array, see the comment in /api/notification/bulkSendNotificationEmails.ts
+    //     to: [
+    //       {
+    //         email: user.email,
+    //         name: user.name ?? "SIWMA User",
+    //       },
+    //     ],
+    //     params: {
+    //       name: user.name ?? "SIWMA User",
+    //       message: `The sell post <h5>"${updated.name}"</h5> is now "${
+    //         updated.status === "available" ? "Available" : "Sold Out"
+    //       }".<br><br><a href="http://localhost:3000/sell-post/${
+    //         updated.id
+    //       }">Click to check it out.</a>`,
+    //     },
+    //   });
+    // });
+
+    // const emailResponse = await sendNotificationEmail(emailRequestBody);
+    // console.log("Email response:", emailResponse.success);
 
     res.status(200).json(updated);
   } catch (error) {
